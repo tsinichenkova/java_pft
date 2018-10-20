a527c855ad96dd5848d20507bce92864460a9ec6
Unknown <t.sinichenkova@infinnitysolutions.com>
2018-10-20 23:58:14 +0500

    Добавлено описание тестового стенда в конфигурационный файл

--- addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    private Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -18,12 +23,15 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
-
+    public ApplicationManager(String browser){
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (browser.equals(BrowserType.CHROME)) {
@@ -33,12 +41,12 @@ public class ApplicationManager {
         }
         wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
         wd.manage().window().maximize();
-        wd.get("http://localhost:8443/addressbook/");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
--- addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws Exception {
