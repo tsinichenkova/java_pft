aaa813d5c71c40226880ec5c47c9f41cb597d64c
Unknown <t.sinichenkova@infinnitysolutions.com>
2018-10-23 20:20:46 +0500

    Добавлено получение данных из БД

--- addressbook/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,47 +3,89 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 import java.util.Objects;
 
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
     @Expose
+    @Column(name = "firstname")
     private  String firstName;
+
     @Expose
+    @Column(name = "lastname")
     private  String lastName;
+
     @Expose
+    @Column(name = "nickname")
     private  String nickname;
+
     @Expose
+    @Column(name = "address")
+    @Type(type =  "text")
     private  String address;
+
     @Expose
+    @Column(name = "home")
+    @Type(type =  "text")
     private  String homeTelephone;
+
     @XStreamOmitField
-    private  String mobileTelephone;
-    @XStreamOmitField
-    private  String workTelephone;
     @Expose
+    @Column(name = "mobile")
+    @Type(type =  "text")
+    private  String mobileTelephone;
+
+    @XStreamOmitField
+    @Expose
+    @Column(name = "work")
+    @Type(type =  "text")
+    private  String workTelephone;
+
+    @Expose
+    @Column(name = "email")
+    @Type(type =  "text")
     private  String email;
+
     @XStreamOmitField
+    @Transient
     private String allPhones;
+
     @XStreamOmitField
+    @Column(name = "email2")
+    @Type(type =  "text")
     private String email2;
+
     @XStreamOmitField
+    @Column(name = "email3")
+    @Type(type =  "text")
     private String email3;
+
     @XStreamOmitField
+    @Transient
     private String allEmails;
+
     @XStreamOmitField
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
+
     @XStreamOmitField
-    private File photo;
+    @Column(name = "photo")
+    @Type(type =  "text")
+    private String photo;
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getAbsolutePath();
         return this;
     }
 
---
+++ addressbook/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -0,0 +1,56 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+import java.util.List;
+
+@Test
+public class HbConnectionTest {
+
+    private static SessionFactory sessionFactory;
+
+
+    @BeforeClass
+    protected void setUp() throws Exception {
+        try {
+            // Create the SessionFactory from hibernate.cfg.xml
+            sessionFactory = new Configuration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    @Test
+    public void testHbConnectionGroup() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery("from GroupData").list();
+        for (GroupData group : result) {
+            System.out.println(group);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Test
+    public void testHbConnectionContact() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<ContactData> result = session.createQuery("from ContactData where deprecated = '0000-00-00'").list();
+        for (ContactData contact : result) {
+            System.out.println(contact);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+}
