5e3d87648e5b83f90c48e64f01529ad6a229fb08
t.sinichenkova <t.sinichenkova@infinnity.ru>
2019-02-17 14:21:02 +0500

    Добавлен первый тест на авторизацию

---
+++ selenium-java/src/test/java/ru/stqa/pft/selenium/Login.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.selenium;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class Login extends TestBase {
+
+    @Test
+    public void login() {
+        driver.get("http://localhost:8443/litecard/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+    }
+}
--- selenium-java/src/test/java/ru/stqa/pft/selenium/MyFirstTest.java
+++ selenium-java/src/test/java/ru/stqa/pft/selenium/MyFirstTest.java
@@ -1,26 +1,12 @@
 package ru.stqa.pft.selenium;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class MyFirstTest {
-
-    private WebDriver driver;
-    private WebDriverWait wait;
-
-    @Before
-    public void start() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, 10);
-    }
+public class MyFirstTest extends TestBase {
 
     @Test
     public void myFirstTest() {
@@ -29,11 +15,5 @@ public class MyFirstTest {
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
         wait.until(titleIs("webdriver - Поиск в Google"));
 
-    }
-
-    @After
-    public void stop() {
-        driver.quit();
-        driver = null;
     }
 }
