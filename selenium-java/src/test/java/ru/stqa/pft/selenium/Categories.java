a90478fbac835101b784071b469c6dc614966bb5
t.sinichenkova <t.sinichenkova@infinnity.ru>
2019-02-23 16:54:36 +0500

    Добавлено 7 задание

---
+++ selenium-java/src/test/java/ru/stqa/pft/selenium/Categories.java
@@ -0,0 +1,46 @@
+package ru.stqa.pft.selenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Categories extends TestBase {
+
+    @Test
+    public void categories() {
+        driver.get("http://localhost:8443/litecard/admin/login.php");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
+
+        List<WebElement> menuItems = driver.findElements(By.xpath("//*[@id=\"app-\"]"));
+
+        List nameMenu = new ArrayList();
+        for (WebElement element : menuItems) {
+            nameMenu.add(element.getText());
+        }
+
+        for (int i = 0; i < menuItems.size(); i++) {
+            driver.findElement(By.xpath("//*[@id=\"app-\"]/a/span[text() = '" + nameMenu.get(i) + "']")).click();
+            areElementsPresent(By.xpath("//*[@id=\"content\"]/h1"));
+
+            List<WebElement> menuItemsLi = driver.findElements(By.xpath("//*[@id=\"app-\"]/ul//span"));
+            List nameCategories = new ArrayList();
+            for (WebElement element : menuItemsLi) {
+                nameCategories.add(element.getText());
+            }
+
+            for (int j = 0; j < menuItemsLi.size(); j++) {
+                driver.findElement(By.xpath("//*[@id=\"app-\"]/ul//span[text()='" + nameCategories.get(j) + "']")).click();
+                areElementsPresent(By.xpath("//*[@id=\"content\"]/h1"));
+            }
+        }
+    }
+}
--- selenium-java/src/test/java/ru/stqa/pft/selenium/TestBase.java
+++ selenium-java/src/test/java/ru/stqa/pft/selenium/TestBase.java
@@ -2,18 +2,13 @@ package ru.stqa.pft.selenium;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxBinary;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.File;
 
 public class TestBase {
 
@@ -28,15 +23,15 @@ public class TestBase {
             wait = new WebDriverWait(driver, 10);
             return;
         }
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("start-fullscreen");
-//        DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setCapability(ChromeOptions.CAPABILITY, options);
-//        System.out.println(((HasCapabilities) driver).getCapabilities());
-//        driver = new ChromeDriver(caps);
-        FirefoxOptions options = new FirefoxOptions();
-        options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")));
-        driver = new FirefoxDriver(options);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-fullscreen");
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(ChromeOptions.CAPABILITY, options);
+        driver = new ChromeDriver();
+        System.out.println(((HasCapabilities) driver).getCapabilities());
+//        FirefoxOptions options = new FirefoxOptions();
+//        options.setBinary(new FirefoxBinary(new File("C:\\Program Files\\Mozilla Firefox\\firefox.exe")));
+//        driver = new FirefoxDriver(options);
         wait = new WebDriverWait(driver, 10);
         tlDriver.set(driver);
 
@@ -48,5 +43,9 @@ public class TestBase {
     public void stop() {
         //driver.quit();
         //driver = null;
+    }
+
+    public boolean areElementsPresent(By locator) {
+        return driver.findElements(locator).size() > 0;
     }
 }
